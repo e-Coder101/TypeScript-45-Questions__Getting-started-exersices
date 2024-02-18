5183e8617e72ce79b231c78bcd78231a679d7138
Ebad ur Rehman <e.codes101@gmail.com>
2024-02-18 19:32:18 +0500

    Ex6 complete

---
+++ Ex6 Stripping Names/app.ts
@@ -0,0 +1,5 @@
+let personName: string = "\n\t\tEbad\n";
+
+console.log(`Hello, Mr.${personName} What do you plan to do today?`);
+
+console.log(`Hello, Mr.${personName.trim()} What do you plan to do today?`);
